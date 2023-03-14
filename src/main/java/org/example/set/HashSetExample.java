d5145c538322447202c34653a46cb826c12276dc
omgshalihin <shalihinsgp@gmail.com>
2023-03-14 17:06:07 +0100

    HashSet

---
+++ src/main/java/org/example/set/HashSetExample.java
@@ -0,0 +1,50 @@
+package org.example.set;
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+public class HashSetExample {
+    // no order
+    // no duplicates
+
+    public static void main(String[] args) {
+
+        Set<Ball> balls = new HashSet<>();
+        balls.add(new Ball("blue"));
+        balls.add(new Ball("blue"));
+        balls.add(new Ball("yellow"));
+        balls.add(new Ball("red"));
+
+        System.out.println("total balls: " + balls.size());
+        balls.forEach(System.out::println);
+
+    }
+
+//    static record Ball(String color){}
+
+    static class Ball{
+        String color;
+
+        public Ball(String color) {
+            this.color = color;
+        }
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Ball ball = (Ball) o;
+            return Objects.equals(color, ball.color);
+        }
+        @Override
+        public int hashCode() {
+            return Objects.hash(color);
+        }
+        @Override
+        public String toString() {
+            return "Ball{" +
+                    "color='" + color + '\'' +
+                    '}';
+        }
+    }
+}
